3c05980ba3e3a39f9f9b926a9124c7d6f0104b18
Taras Glek <taras@glek.net>
2020-12-09 19:50:52 -0800

    passive ver

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This is based on $4 ble sensors from https://www.youtube.com/watch?v=NXKzFG61lNs&feature=youtu.be
+This is based on $4 ble sensors from https://www.youtube.com/watch?v=NXKzFG61lNs&feature=youtu.bebluetooth_utils.py from https://github.com/colin-guyon/py-bluetooth-utils
+code bones from https://hackaday.com/2020/12/08/exploring-custom-firmware-on-xiaomi-thermometers/
--- server.py
+++ server.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+import sys
+from datetime import datetime
+import bluetooth._bluetooth as bluez
 import http.server
 import re
-import sys
 import time
+from bluetooth_utils import (toggle_device, enable_le_scan,
+                             parse_le_advertising_events,
+                             disable_le_scan, raw_packet_to_str)
+ 
 
 class Prometheus(http.server.SimpleHTTPRequestHandler):
     stats = {}
@@ -29,7 +35,7 @@ class Prometheus(http.server.SimpleHTTPRequestHandler):
         httpd = socketserver.TCPServer(('', 9100), Prometheus)
         threading.Thread(target=lambda:httpd.serve_forever()).start()
 
-p = re.compile(".*Service Data \(UUID 0x181a\): (............)..(..)(..)(..)(..)")
+Prometheus.run()
 
 thermometers = {
     'a4c13885878e': 'garage',
@@ -37,18 +43,40 @@ thermometers = {
     'a4c138863d06': 'basement',
     'a4c138946fa0': 'upstairs'
 }
-Prometheus.run()
-for line in sys.stdin:
-    line = line.rstrip()
-    m = p.match(line)
-    if not m:
-        continue
-    mac= m.groups()[0]
-    (temp, humidity, battery, voltage) = map(lambda x:int(x, 16), m.groups()[1:])
-    temp = temp/10.0
-    name = thermometers.get(mac, mac)
-    payload = {'temperature':temp, 'humidity':humidity, 'battery': battery, 'voltage':voltage}
-    payload['expiration'] = time.time() + 120
-    Prometheus.stats[name]=(payload)
-    print(name)
-    
+# Use 0 for hci0
+dev_id = 0
+toggle_device(dev_id, True)
+ 
+try:
+    sock = bluez.hci_open_dev(dev_id)
+except:
+    print("Cannot open bluetooth device %i" % dev_id)
+    raise
+ 
+# Set filter to "True" to see only one packet per device
+enable_le_scan(sock, filter_duplicates=False)
+p = re.compile("(............)..(..)(..)(..)(..)")
+
+try:
+    def le_advertise_packet_handler(mac, adv_type, data, rssi):
+        data_str = raw_packet_to_str(data)
+        # Check for ATC preamble
+        if data_str[6:10] == '1a18':
+            m = p.match(data_str[10:])
+            mac= m.groups()[0]
+            (temp, humidity, battery, voltage) = map(lambda x:int(x, 16), m.groups()[1:])
+            temp = temp/10.0
+            name = thermometers.get(mac, mac)
+            payload = {'temperature':temp, 'humidity':humidity, 'battery': battery, 'voltage':voltage}
+            payload['expiration'] = time.time() + 120
+            Prometheus.stats[name]=(payload)
+            print("%s - %s Temp: %sc Humidity: %s%% Batt: %s%%" % \
+                 (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), name, temp, humidity, battery))
+ 
+    # Called on new LE packet
+    parse_le_advertising_events(sock,
+                                handler=le_advertise_packet_handler,
+                                debug=False)
+# Scan until Ctrl-C
+except KeyboardInterrupt:
+    disable_le_scan(sock)
